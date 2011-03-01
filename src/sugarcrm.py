74d3a9c46e525a0dc0e6311c784c380169a300fb
Andrew Kubera <andrewkubera@ip-131-123-182-176.wpa3.kent.edu>
2011-03-01 09:53:14 -0500

    added sendRequst method to sugarcrm class

--- src/sugarcrm.py
+++ src/sugarcrm.py
@@ -62,22 +62,14 @@ class Sugarcrm:
 
     ## Login function to estabilsh connection with a server
     # @param username string of sugarcrm user
-                # @param
+    # @param password plaintext string of the users password
     def login(self, username, password):
         data = {'user_auth' : {'user_name' : username, 'password' : passencode(password)}}
         args = {'method': 'login', 'input_type': 'JSON', 'response_type' : 'JSON', 'rest_data' : data}
         params = urllib.urlencode(args)
-        response = urllib.urlopen(self.host, params)
-        try:
-            x = json.load(response)
-        except TypeError:
-            raise InvalidConnection
-
-        try:
-            self.testForError(x)
-        except GeneralException:
-            raise InvalidLogin
-
+        
+        x = self.sendRequest(params)
+        
         try:
             print "ID: "+x["id"]
             self.id = x["id"]
@@ -87,6 +79,22 @@ class Sugarcrm:
         # If all goes well we've successfully connected
         self.connected = 1
 
+    ## sendRequest sends all requests to the server
+    # @param params parameters to the function being called, should be urlencoded (eg. params = urllib.urlencode(args))
+    # @return dictionary object of server response 
+    def sendRequest(self, params):
+        response = urllib.urlopen(self.host, params)
+        try:
+            x = json.load(response)
+        except TypeError:
+            raise InvalidConnection
+        
+        try:
+            self.testForError(x)
+        except GeneralException:
+            raise InvalidLogin
+        return x
+                
     ## Test any returned object for an error
     # @param self the object pointer
     # @param obj the object which will be identified as an error or not
