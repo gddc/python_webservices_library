dbd5f5a944b7bdaa1fd9555df64f36aca81b93e6
Luis Barrueco <luis.barrueco@gmail.com>
2012-08-01 11:44:18 -0300

    Fixed __init__.py for new version

--- sugarcrm/__init__.py
+++ sugarcrm/__init__.py
@@ -1,11 +1,8 @@
 
 from sugarcrm import *
-from sugarbean import *
-from sugarentrylist import *
 from sugarmodule import *
-from sugar_error import *
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # This is supposed to help importing, does nothing as far as I can tell
-__ALL__ = ['sugarcrm','sugarbean']
+__ALL__ = ['sugarcrm']
 
