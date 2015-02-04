9a0b4b20aee5b629f305b739fc120281f6e53144
Daniel Casper <dancasper@gmail.com>
2015-02-04 10:27:32 -0700

    Version Increment.
    Increase version to 0.2.2.  Reflected correctly
    in both __init__ and setup.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 import os
 from distutils.core import setup
 
-version = '0.2.0'
+version = '0.2.2'
 
 PACKAGES = ['sugarcrm']
 
--- sugarcrm/__init__.py
+++ sugarcrm/__init__.py
@@ -1,7 +1,7 @@
 
 from sugarcrm import *
 from sugarmodule import *
-__version__ = "0.1.1"
+__version__ = "0.2.2"
 
 # This is supposed to help importing, does nothing as far as I can tell
 __ALL__ = ['sugarcrm']
