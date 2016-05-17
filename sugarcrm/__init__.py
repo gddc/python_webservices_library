d6403052057034268a84306c2bf20c3e5700201d
Daniel Casper <dancasper@gmail.com>
2016-05-17 10:37:46 -0600

    Increment version to 0.3.3.

--- sugarcrm/__init__.py
+++ sugarcrm/__init__.py
@@ -1,6 +1,6 @@
 from sugarcrm import *
 from sugarmodule import *
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # This is supposed to help importing, does nothing as far as I can tell
 __ALL__ = ['sugarcrm']
